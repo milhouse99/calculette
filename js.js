d06770c734f1e717875213e73df5ab57857d368f
milhouse99 <efehmartinez@gmail.com>
2018-06-18 11:26:03 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+
+function c(val){
+    document.getElementbyId("display").value =val;
+    }
+    
+        function math (val){
+            document.getElementbyId("display").value +=val;
+        }
+
+function e () {
+    try {
+    c (eval(getElementbyId("display").value));
+}
+catch{
+    (e)
+
+}
+c ('Error');
+}
